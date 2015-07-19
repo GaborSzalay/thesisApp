f7db2801751ee7fb81d80bdf442ce65e8d530dd1
Gabor Szalay <szalay.gabor0@gmail.com>
2015-07-19 15:59:36 +0200

    #10: exclude registration urls from security area

--- src/main/java/com/university/thesisapp/web/provider/UrlProvider.java
+++ src/main/java/com/university/thesisapp/web/provider/UrlProvider.java
@@ -5,7 +5,6 @@ package com.university.thesisapp.web.provider;
  */
 public class UrlProvider {
     public static final String HOME_PAGE_URL = "/";
-    public static final String INDEX_URL = "/index.html";
     public static final String REGISTRATION_URL = "/registration.html";
     public static final String CREATE_ACCOUNT_URL = "/create_account.html";
 }
